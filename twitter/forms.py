55fe8fd188b65fc9ee2bab118b518fbf03146ff4
Tolgahan ÜZÜN <mail@tolgahanuzun.com>
2016-08-12 02:48:55 +0300

    new forms create

---
+++ twitter/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from .models import Populer,Kac_Kullanici,Rt_Kullanicimodel,Rt_Kacmodel
+
+class Pupuler_Kullanici(forms.ModelForm):
+
+	class Meta:
+		model = Populer
+		fields = ('twitter_name',)
+
+class Kac_Kullan(forms.ModelForm):
+
+	class Meta:
+		model = Kac_Kullanici
+		fields = ('insan_limiti',)
+
+class Rt_Kullanici(forms.ModelForm):
+
+	class Meta:
+		model = Rt_Kullanicimodel
+		fields = ('rt_name',)
+
+class Rt_Kullan(forms.ModelForm):
+
+	class Meta:
+		model = Rt_Kacmodel
+		fields = ('rt_limiti',)
