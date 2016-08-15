3ba4473fb34fea06c2a6f57532f917f0b6429950
Tolgahan ÜZÜN <mail@tolgahanuzun.com>
2016-08-15 03:16:07 +0300

    img url form and result form add

--- tumblr/forms.py
+++ tumblr/forms.py
@@ -1,8 +1,17 @@
 from django import forms
-from .models import Like_Limit
+from .models import Like_Limit,Img_Post
 
 class Like(forms.ModelForm):
 
 	class Meta:
-		model = Like_Limit
+		model  = Like_Limit
 		fields = ('like',)
+
+class Img_Post_f(forms.ModelForm):
+
+	class Meta:
+		model  = Img_Post
+		fields = ('urllink',)
+
+class Img_Post_push(forms.Form):
+	 comment = forms.CharField()
