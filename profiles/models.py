a7b8ae897e0a68445a3f13a6cb2a3321bb02e9a8
Tolgahan ÜZÜN <mail@tolgahanuzun.com>
2016-08-16 02:26:16 +0300

    api keys requirement fields add

--- profiles/models.py
+++ profiles/models.py
@@ -6,6 +6,12 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
 	user = models.OneToOneField(User)
+	
+	consumer_keys = models.CharField(max_length=120,null=True ,blank=True)
+	consumer_secrets = models.CharField(max_length=120,null=True ,blank=True)
+	access_tokens = models.CharField(max_length=120,null=True ,blank=True)
+	access_token_secrets = models.CharField(max_length=120,null=True ,blank=True)
+
 
 	def __unicode__(self):
 		return "%s" % (self.user)
