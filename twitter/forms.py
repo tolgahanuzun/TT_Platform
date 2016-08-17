1be8c4dcfa3abb63804f240702168ed0743bca6d
Tolgahan ÜZÜN <mail@tolgahanuzun.com>
2016-08-17 14:59:39 +0300

    create unfollow forms

--- twitter/forms.py
+++ twitter/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Populer,Kac_Kullanici,Rt_Kullanicimodel,Rt_Kacmodel
+from .models import Populer,Kac_Kullanici,Rt_Kullanicimodel,Rt_Kacmodel,Unf_model
 
 class Pupuler_Kullanici(forms.ModelForm):
 
@@ -24,3 +24,13 @@ class Rt_Kullan(forms.ModelForm):
 	class Meta:
 		model = Rt_Kacmodel
 		fields = ('rt_limiti',)
+
+
+class Unf_form(forms.ModelForm):
+
+	class Meta:
+		model = Unf_model
+		fields = ('unf',)
+
+
+
