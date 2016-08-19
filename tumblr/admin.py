edbf5e9c4f430e3a5a16e8d04d9591baf6a0eb27
Tolgahan ÜZÜN <mail@tolgahanuzun.com>
2016-08-19 04:22:28 +0300

    Show new models fields

--- tumblr/admin.py
+++ tumblr/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Img_Post
+from .models import Img_Post,Rt_Put
 
 admin.site.register(Img_Post)
+admin.site.register(Rt_Put)
