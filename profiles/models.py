31b2ead74dfea103a85b449cc3acb8344e88d342
Tolgahan ÜZÜN <mail@tolgahanuzun.com>
2016-08-21 04:06:52 +0300

    PEP8 update and new field add

--- profiles/models.py
+++ profiles/models.py
@@ -7,10 +7,14 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
 	user = models.OneToOneField(User)
 	
-	consumer_keys = models.CharField(max_length=120,null=True ,blank=True)
-	consumer_secrets = models.CharField(max_length=120,null=True ,blank=True)
-	access_tokens = models.CharField(max_length=120,null=True ,blank=True)
-	access_token_secrets = models.CharField(max_length=120,null=True ,blank=True)
+	tumblr_consumer_keys = models.CharField(max_length=120,null=True ,blank=True)
+	tumblr_consumer_secrets = models.CharField(max_length=120,null=True ,blank=True)
+	tumblr_access_tokens = models.CharField(max_length=120,null=True ,blank=True)
+	tumblr_access_token_secrets = models.CharField(max_length=120,null=True ,blank=True)
+	twitter_consumer_keys = models.CharField(max_length=120,null=True ,blank=True)
+	twitter_consumer_secrets = models.CharField(max_length=120,null=True ,blank=True)
+	twitter_access_tokens = models.CharField(max_length=120,null=True ,blank=True)
+	twitter_access_token_secrets = models.CharField(max_length=120,null=True ,blank=True)
 
 
 	def __unicode__(self):
