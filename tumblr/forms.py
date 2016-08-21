4e6c941e9bf8db5000bdf8954300b927b18883f2
Tolgahan ÜZÜN <mail@tolgahanuzun.com>
2016-08-21 04:10:27 +0300

    Update and PEP8

--- tumblr/forms.py
+++ tumblr/forms.py
@@ -7,31 +7,31 @@ class Like(forms.ModelForm):
 		model  = Like_Limit
 		fields = ('like',)
 
-class Img_Post_f(forms.ModelForm):
+class Img_Post_Get(forms.ModelForm):
 
 	class Meta:
 		model  = Img_Post
 		fields = ('urllink',)
 
-class Img_Post_push(forms.ModelForm):
+class Img_Post_Push(forms.ModelForm):
 
 
 	class Meta:
 		model  = Img_Content
 		fields = ('blogname','context','tag',) 
 
-class Post_Rt_Put(forms.ModelForm):
+class Rt_Post_Put(forms.ModelForm):
 
 	class Meta:
 		model = Rt_Put
-		fields = ('rt_link',)
+		fields = ('rtlink',)
 
 
-class Post_Rt_Push(forms.ModelForm):
+class Rt_Post_Push(forms.ModelForm):
 
 	class Meta:
 		model = Rt_Push
 		fields = ('blogname','context',)
 
-class T_Url_follow(forms.Form):
+class Url_Follow(forms.Form):
     urlname = forms.CharField(label='Url', max_length=200)
