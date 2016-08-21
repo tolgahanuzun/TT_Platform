850726d95254ef7a84bab63de64b1f813fd7424a
Tolgahan ÜZÜN <mail@tolgahanuzun.com>
2016-08-21 04:09:27 +0300

    new func create and fixed

--- profiles/views.py
+++ profiles/views.py
@@ -6,7 +6,7 @@ from django.core.exceptions import ValidationError
 from django.http import *
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
-from profiles.forms import LoginForm, RegistrationForm, KeyCreateForm
+from profiles.forms import LoginForm, RegistrationForm, Tumblr_Key_Create,Twitter_Key_Create
 from profiles.models import Profile
 from django.utils import timezone
 
@@ -17,16 +17,19 @@ def login(request):
 			username = request.POST['username']
 			password = request.POST['password']
 			user = authenticate(username=username, password=password)
+
 			if user is not None:
 				auth_login(request, user)
 				return HttpResponseRedirect("/")
 			else:
 				context['error'] = 'Non active user'
+				
 		except:
 			context['error'] = ''
 
 		populateContext(request, context)
 		return render(request, 'login.html', context)
+
 	else:
 		return HttpResponseRedirect("/index/")
 
@@ -43,6 +46,7 @@ def logout(request):
 
 def populateContext(request, context):
 	context['authenticated'] = request.user.is_authenticated()
+
 	if context['authenticated'] == True:
 		context['username'] = request.user.username
 
@@ -54,6 +58,7 @@ def register(request):
 
 		if request.method == "POST":
 			form = RegistrationForm(request.POST)
+
 			if  form.is_valid():
 				form.save()       
 				return render(request,"register.html",
@@ -65,36 +70,58 @@ def register(request):
 		else:
 			form = RegistrationForm()
 			return render(request, "register.html",{'form':form})
+
 	else:
 		return HttpResponseRedirect("/index/")    
 
 def home(request):
 	if request.user.is_authenticated():
-
 		currentTime = timezone.localtime(timezone.now())
-		
-
-
 		return render(request, "index.html", 
 					  {'currentTime':currentTime})
 	else:
 		return render(request, "index.html")
 
-def keyupdate(request):
+def tumblr_key(request):
 	if request.user.is_authenticated():
-		form = KeyCreateForm()
+		form = Tumblr_Key_Create()
+
 		if request.method == "POST":
-			form = KeyCreateForm(request.POST, user=request.user)
+			form = Tumblr_Key_Create(request.POST, user=request.user)
+
 			if form.is_valid():
 				form.change()
+				return render(request,'user_key.html',{'tumblr_update':""})
 
-				return render(request,'user_key.html',{'update':""})
 			else:
-				return render(request,'user_key.html',{'form': form})	
+				return render(request,'user_key.html',{'tumblr_form': form})
+
 		else:			
-			return render(request,'user_key.html',{'form': form})		
+			return render(request,'user_key.html',{'tumblr_form': form})	
+
 	else:
 		return render(request,'index.html')
+
+def twitter_key(request):
+	if request.user.is_authenticated():
+		form = Twitter_Key_Create()
+
+		if request.method == "POST":
+			form = Twitter_Key_Create(request.POST, user=request.user)
+
+			if form.is_valid():
+				form.change()
+				return render(request,'user_key.html',{'twitter_update':""})
+
+			else:
+				return render(request,'user_key.html',{'twitter_form': form})
+
+		else:			
+			return render(request,'user_key.html',{'twitter_form': form})
+
+	else:
+		return render(request,'index.html')
+
 
 
 
