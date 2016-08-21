bfdfd08ab1c7dc0efa43da5d5062028c0a13afa6
Tolgahan ÜZÜN <mail@tolgahanuzun.com>
2016-08-21 04:10:08 +0300

    PEP8

--- tumblr/admin.py
+++ tumblr/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
-from .models import Img_Post,Rt_Put
+from .models import Img_Post,Rt_Put,Rt_Push
 
 admin.site.register(Img_Post)
 admin.site.register(Rt_Put)
+admin.site.register(Rt_Push)
