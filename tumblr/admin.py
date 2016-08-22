318894d3be81e0b8bab1c5460a90b1fe88780e02
Tolgahan ÜZÜN <mail@tolgahanuzun.com>
2016-08-23 01:43:42 +0300

    new table add

--- tumblr/admin.py
+++ tumblr/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Img_Post,Rt_Put,Rt_Push
+from .models import Img_Post,Rt_Put,Rt_Push,Img_Content
 
 admin.site.register(Img_Post)
 admin.site.register(Rt_Put)
 admin.site.register(Rt_Push)
+admin.site.register(Img_Content)
+
