bf90e89e457a79c55952b3db21d31431d3e96d51
tolgahanuzun <mail@tolgahanuzun.com>
2016-09-16 23:35:20 +0300

    js files update

--- static/js/materialize/init.js
+++ static/js/materialize/init.js
@@ -5,3 +5,10 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+$(document).ready(function(){
+$('.parallax').parallax();
+});
